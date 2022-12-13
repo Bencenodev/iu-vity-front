5d80c8cb1cab9e32bf30266a418a58024cabda22
Robinson <robsoncol1947@gmail.com>
2022-12-13 09:54:14 -0500

    correction interview

--- src/app/service/product-service.service.ts
+++ src/app/service/product-service.service.ts
@@ -1,35 +1,36 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { ProductoRequestDTO } from '../model/rquestProduct';
-import { ProductoResponseDTO } from '../model/responseProduct';
+import { url_base } from '../constants/constants';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductServiceService {
 
-  baseURL = "http://localhost:5000/api/v1/"
 
   constructor(private http: HttpClient) { }
 
   getProduct(id: string){
-    return this.http.get(this.baseURL+'producto/'+id);
+    return this.http.get(url_base+'producto/'+id);
   }
 
   saveProduct(product:ProductoRequestDTO){
     const headers = { 'content-type': 'application/json'}  
     const body=JSON.stringify(product);
-    return this.http.post(this.baseURL+"producto",body,{'headers':headers});
+    return this.http.post(url_base+"producto",body,{'headers':headers});
   } 
   
   updateProduct(id: string, product:ProductoRequestDTO){
     const headers = { 'content-type': 'application/json'}  
     const body=JSON.stringify(product);
-    return this.http.put(this.baseURL+"producto/"+id,body,{'headers':headers});
+    return this.http.put(url_base+"producto/"+id,body,{'headers':headers});
   }
 
   findAllProduct(){
-    return this.http.get(this.baseURL+'productos');
+    return this.http.get(url_base+'productos');
   }
   
 }
